6e4c62540cddf80ccf2a1516a4cf0b9bd08eb376
Lucian Andercou <lucian.andercou@gmail.com>
2017-01-28 15:31:30 +0200

    Use facebook API to gather facebook events

---
+++ facebook_scrape_events.py
@@ -0,0 +1,41 @@
+import urllib.request
+import json
+import datetime
+import time
+
+app_id = "185295105255499"
+app_secret = "ad34ee7526cb3521cb0453dc8b339098" # DO NOT SHARE WITH ANYONE!
+page_id = "TrooperRocks"
+
+access_token = app_id + "|" + app_secret
+
+def request_until_succeed(url):
+    req = urllib.request.Request(url)
+    success = False
+    while success is False:
+        try:
+            response = urllib.request.urlopen(req)
+            if response.getcode() == 200:
+                success = True
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+
+            print("Error for URL %s: %s" % (url, datetime.datetime.now()))
+            print("Retrying.")
+
+    return response.read().decode(response.headers.get_content_charset())
+
+def scrapeFacebookEvents(page_id, access_token):
+
+    # Construct the URL string; see http://stackoverflow.com/a/37239851 for
+    # Reactions parameters
+    url = 'https://graph.facebook.com/v2.6/'+page_id+'/events?access_token='+access_token
+
+    # retrieve data
+    data = json.loads(request_until_succeed(url))
+
+    return data
+
+if __name__ == '__main__':
+    print(scrapeFacebookEvents(page_id, access_token))
